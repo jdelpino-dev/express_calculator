f7ca785c1211a927af634772e578bf7dd916132b
José Delpino <delpinoivivas@gmail.com>
2024-05-16 10:31:05 -0500

    feat: Add validation middleware for query parameter 'nums'

--- source/middleware/validateNums.js
+++ source/middleware/validateNums.js
@@ -2,3 +2,25 @@
  * @file validateNums.js
  * @description Middleware to validate the query parameter `nums` for statistical routes.
  */
+
+const validateNums = (req, res, next) => {
+  if (!req.query.nums) {
+    return res.status(400).json({
+      error:
+        "A number or multiple numbers must be provided in the query string nums argument.",
+    });
+  }
+
+  const nums = req.query.nums.split(",").map(Number);
+
+  if (nums.some(isNaN)) {
+    return res
+      .status(400)
+      .json({ error: "All elements in nums must be numbers." });
+  }
+
+  req.nums = nums;
+  next();
+};
+
+export default validateNums;
