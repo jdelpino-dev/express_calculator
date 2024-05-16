175e7736518f6ce6c40be9cfce5503f47092070d
José Delpino <delpinoivivas@gmail.com>
2024-05-16 10:47:18 -0500

    feat: Add saveToFile utility function

--- source/utils/saveToFile.js
+++ source/utils/saveToFile.js
@@ -2,3 +2,18 @@
  * @file saveToFile.js
  * @description Utility function to save results to a file with a timestamp.
  */
+
+import { writeFile } from "fs/promises";
+
+const saveToFile = async (filename, data) => {
+  const timestamp = new Date().toISOString();
+  const fileContent = { timestamp, ...data };
+
+  try {
+    await writeFile(filename, JSON.stringify(fileContent, null, 2));
+  } catch (error) {
+    console.error("Error writing to file", error);
+  }
+};
+
+export default saveToFile;
