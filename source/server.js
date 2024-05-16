874b254a47e3d3353313c5a80f25da5d02e8c39b
José Delpino <delpinoivivas@gmail.com>
2024-05-16 11:02:42 -0500

    feat: Implement server.js

--- source/server.js
+++ source/server.js
@@ -2,3 +2,11 @@
  * @file server.js
  * @description Starts the Express server on the specified port.
  */
+
+import app from "./app.js";
+
+const PORT = 3300;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
