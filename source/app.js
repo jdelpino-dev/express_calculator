9127bc5441404a77c3ad825e3e96ab7d3f85e905
José Delpino <delpinoivivas@gmail.com>
2024-05-16 11:01:17 -0500

    chore: Refactor app.js to improve code organization and error handling

--- source/app.js
+++ source/app.js
@@ -1,4 +1,42 @@
 /**
  * @file app.js
- * @description Configures the Express application, including middleware for logging, JSON parsing, and error handling.
+ * @description Configures the Express application, including middleware
+ * for logging, JSON parsing, and error handling.
  */
+
+import express from "express";
+import morgan from "morgan";
+import statsRouter from "./routes/stats.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/stats", statsRouter);
+
+app.use((err, req, res, next) => {
+  if (err.type === "entity.parse.failed") {
+    res.status(400).json({ error: "Invalid JSON" });
+  } else {
+    next(err);
+  }
+});
+
+// Generic error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  const statusCode = err.status || 500;
+  res.status(statusCode);
+
+  if (statusCode === 500) {
+    return res.json({
+      error: "Internal Server Error",
+    });
+  }
+
+  return res.json({
+    error: err.message,
+  });
+});
+
+export default app;
